9913953e0c3e32b262a73eb00a794cd105aa0c97
fioralda <fioraldaosmenai99@gmail.com>
2021-08-11 15:02:12 +0300

    fix cards & adding icon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -27,6 +28,7 @@ import { HttpClientModule } from '@angular/common/http';
     MatProgressSpinnerModule,
     MatCardModule,
     MatGridListModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
