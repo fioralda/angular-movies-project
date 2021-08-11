e5e20929f3d0706755375876e8fceecadca746b6
fioralda <fioraldaosmenai99@gmail.com>
2021-08-11 16:01:12 +0300

    fix routing

--- src/app/components/movie-details/movie-details.component.ts
+++ src/app/components/movie-details/movie-details.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-movie-details',
   templateUrl: './movie-details.component.html',
-  styleUrls: ['./movie-details.component.css']
+  styleUrls: ['./movie-details.component.css'],
 })
 export class MovieDetailsComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  hasRoute(route: string) {
+    return this.router.url === route;
   }
-
 }
--- src/app/movies.service.ts
+++ src/app/movies.service.ts
@@ -11,4 +11,6 @@ export class MoviesService {
     const api_url = `https://api.themoviedb.org/3/search/movie?api_key=85204a8cc33baf447559fb6d51b18313&language=en-US&page=${page}&include_adult=false&query=${searchTerm}`;
     return this.http.get(api_url);
   }
+
+  getMovieDetails(id) {}
 }
